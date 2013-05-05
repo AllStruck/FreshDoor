752f766c8966b393a7de000d7a4828e6360bd6f5
iwantdavid <github@allstruck.com>
2013-05-05 01:38:46 +0000

    Delete DS_Store.

--- ReadMe.md
+++ ReadMe.md
@@ -1,14 +1,13 @@
-### FreshDoor ###
+# FreshDoor #
 
 __This is a work in progress.__
 
 The first version of this plugin should:
-* Add a WordPress settings page.
-* Allow user to enter FreshBooks account name and API key.
-* Allow user to synchronize FreshBooks data (items and tasks) to WordPress.
-* Allow user to activate a webhook for autosync whenever something happens remotely on FreshBooks.
-* Allow user to toggle sync options:
+* Allow admin to enter FreshBooks account name and API key.
+* Allow admin to synchronize FreshBooks data (items and tasks) to WordPress.
+* Allow admin to activate a webhook for autosync whenever something happens remotely on FreshBooks.
+* Allow admin to toggle sync options:
 	* Delete from FB on delete from WP.
 	* Add to FB on add from WP.
 	* Delete from WP on delete from FB.
-	* Add to WP on add from FB.
+	* Add to WP on add from FB. 
--- freshdoor.php
+++ freshdoor.php
@@ -6,16 +6,35 @@ Description: A WordPress plugin to connect with and sync to/from FreshBooks usin
 Version: 1.0
 Author: AllStruck
 Author URI: http://allstruck.com/
-Copyright: 2012, AllStruck
+Copyright: 2013, AllStruck
 */
 
+/*
+	TODO:
+		- Add checkbox settings to enable checked CPTs as WooCommerce Products.
+		- Inital sync from FreshBooks -> WordPress CPT (items and tasks).
+		- Add field on WooCommerce Products admin to map to FreshBooks Item/Task.
+		- Sync to FreshBooks when WP CPTs changes.
+		- Add a webhook from FreshBooks -> WordPress.
+		- Sync to WordPress CTPs when FB changes.
+		- Add FreshBooks as a payment gateway in WooCommerce.
+		- Create unpaid invoice with WooCommerce checkout using our custom gateway.
+		- Create paid invoice with WooCommerce checkout using other gateway.
+		- Add method for uninstall (remove all data).
+*/
+
+	define('FRDO_PATH', plugin_dir_path( __FILE__ ));
+	define('FRDO_URL', plugin_dir_url( __FILE__ ));
+
 /* Include FreshLibPHP API */
-	require_once "library/FreshLibPHP/Callback.php";
-	require_once "library/FreshLibPHP/Client.php";
+	require_once FRDO_PATH . '/library/FreshLibPHP/Callback.php';
+	require_once FRDO_PATH . 'library/FreshLibPHP/Client.php';
 
 /* Include this app */
-	require_once "library/app/app.php";
+	require_once FRDO_PATH . 'library/app/app.php';
+	require_once FRDO_PATH . 'library/app/settings.php';
+	require_once FRDO_PATH . 'library/app/post-types.php';
 
-$FreshDoor = new FreshDoor();
-
-?>
+$FD = new FreshDoor();
+$FDS = new FreshDoorSettings();
+$FDPT = new FreshDoorPostTypes();
